3a3b3a12d1eb23597ad732839c96e6f6334961b9
Nipuna Chhabra <chhabra.nipuna@gmail.com>
2021-07-29 21:58:06 +0530

    week2

--- node-express/index.js
+++ node-express/index.js
@@ -1,17 +1,47 @@
-const express = require('express'),
-     http = require('http');
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-const hostname = 'localhost';
-const port = 3000;
+
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
+const hostname = 'localhost',
+      port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promos', promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+// app.all('/dishes', (req,res,next)=>{
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type','text/plain');
+//   next();
+// });
+
+// app.get('/dishes',(req,res,next)=>{
+//     res.end('Will send all the dishes to you!');
+// });
+
+// app.get('/dishes/:dishId', (req,res,next) => {
+//   res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+// });
+
+
 
 app.use((req, res, next) => {
-  console.log(req.headers);
+  //console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
 
 const server = http.createServer(app);
