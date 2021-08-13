92944a9eb33af738884adaf4970168399198796e
Nipuna Chhabra <chhabra.nipuna@gmail.com>
2021-08-14 01:23:08 +0530

    passport jwt

--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
 
 const Promotions = require('../models/promotions');
 
@@ -20,7 +21,7 @@ promoRouter.route('/')
     .catch((err)=>next(err));
   })
   
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
     Promotions.create(req.body)
     .then((promotion) => {
       res.statusCode = 200;
@@ -29,12 +30,12 @@ promoRouter.route('/')
     },(err)=>next(err))
     .catch((err)=>next(err));
   })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promos');
   })
    
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
   Promotions.remove(req.body)
   .then((promotion) => {
     res.statusCode = 200;
@@ -45,7 +46,7 @@ promoRouter.route('/')
 });
 
 promoRouter.route('/:promoId')
-.get((req,res,next) => {
+.get(authenticate.verifyUser, (req,res,next) => {
   Promotions.findById(req.params.promotionId)
     .then((promotion) => {
         res.statusCode = 200;
@@ -55,12 +56,12 @@ promoRouter.route('/:promoId')
     .catch((err) => next(err));
   })
 
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
 res.statusCode = 403;
 res.end('POST operation not supported on /promos/'+ req.params.promoId);
 })
 
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
   Promotions.findByIdAndUpdate(req.params.promotionId, {
     $set: req.body
 }, { new: true })
@@ -72,7 +73,7 @@ res.end('POST operation not supported on /promos/'+ req.params.promoId);
 .catch((err) => next(err));
 })
 
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
   Promotions.findByIdAndRemove(req.params.promotionId)
   .then((resp) => {
       res.statusCode = 200;
